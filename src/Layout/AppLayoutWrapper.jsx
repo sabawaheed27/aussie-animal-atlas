8f507327d717db4bd4eb5940c49af597f2e1843e
Saba Waheed <saba_sh27@hotmail.com>
2025-06-10 19:31:30 +0200

    fixed small issues

--- src/Layout/AppLayoutWrapper.jsx
+++ src/Layout/AppLayoutWrapper.jsx
@@ -1,28 +1,4 @@
 
-// import { Outlet } from "react-router-dom";
-// import Header from "../components/Header";
-// import Footer from "../components/Footer";
-// import DesktopView from "../Layout/DesktopView";
-// import MobileView from "../Layout/MobileView";
-
-// const AppLayoutWrapper = ({ setShowAnimal, mobileMenu, setMobileMenu, darkMode }) => {
-//   return (
-//     <>
-//       <Header pageTitle="Australian Zoo" darkMode={darkMode} />
-//       <DesktopView setShowAnimal={setShowAnimal}>
-//         <MobileView mobileMenu={mobileMenu} setMobileMenu={setMobileMenu}>
-//           <Outlet />
-//         </MobileView>
-//       </DesktopView>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default AppLayoutWrapper;
-
-
-
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
